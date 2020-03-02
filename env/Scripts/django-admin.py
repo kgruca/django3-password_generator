f4261806a4bf5ea55a845d4f28616260c5de88f2
klis13 <takitamlis89@gmail.com>
2020-03-01 21:36:23 -0800

    This is a password-generator site

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\krzysztof\desktop\developer\password_generator\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
